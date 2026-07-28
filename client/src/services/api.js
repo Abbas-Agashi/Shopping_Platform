cf65c6efdcf2455237f129a417c3e0cee3c3ac85
AbbasAgashi <abbasagashi53@gmail.com>
2026-07-28 15:59:46 +0530

    Fix frontend API URL to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://shopping-platform-wyyh.onrender.com/api';
 
 export const getProducts = (params) => axios.get(`${API_URL}/products`, { params });
 export const getProductById = (id) => axios.get(`${API_URL}/products/${id}`);
